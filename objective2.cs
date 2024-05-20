fe4c90162e46bb9041d950714d61525820f2dd39
Sathish Kumar G <113616023+SathishKumar03082004@users.noreply.github.com>
2024-05-20 19:29:12 +0530

    100%completed

--- objective2.cs
+++ objective2.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class objective2 : MonoBehaviour
 {
+    [Header("Objective")]
+    public Text object2;
+
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player"){
 
-            ObjectivesComplete.occurrence.GetObjectiveDone(true,true,false,false,false);
+            GetObjectiveDone(true);
 
             Destroy(gameObject ,2f);
+        }
+    }
+
+    public void GetObjectiveDone(bool obj2){
+        if(obj2 == true){
+            object2.text = "02. Completed";
+            object2.color = Color.green;
+        }
+        else{
+            object2.text = "02. Locate the villagers";
+            object2.color = Color.white;
         }
     }
 }
